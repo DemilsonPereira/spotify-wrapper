6779128144af2aed9c7ee6ecfa9c7eece7a41e85
Demilson Pereira <demilson.d2102@gmail.com>
2021-07-02 21:20:43 -0400

    Endpoint done the user configuration with tests, using Spotify's GET method.

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const search = (query, type) => {
+  // fetch(`https://api.spotify.com/v1/search?q=Incubus&type=artist`);
+
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
+
+};
+
+const searchAlbums = () => {
+
+};
+
+const searchArtists = () => {
+
+};
+
+const searchTracks = () => {
+
+};
+
+const searchPlaylists = () => {
+
+};
+
+export {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+};
